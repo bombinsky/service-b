7a6daea11d448540bcc66f0d716915a8013ab683
bombinsky <bombinsky@gmail.com>
2020-07-09 20:16:19 +0200

    Pronto

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 EmailTemplate.find_or_create_by!(
   name: 'external_urls_request_results',
-  subject: "External urls in your Twitter home line between {{request_start_time}} and {{request_end_time}}",
+  subject: 'External urls in your Twitter home line between {{request_start_time}} and {{request_end_time}}',
   body: File.open(Rails.root + 'spec/fixtures/external_urls_request_results.liquid').read
 )
